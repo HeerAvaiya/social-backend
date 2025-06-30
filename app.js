ac4d46189ff235cde6710c6f7795839e1580ce40
Heer Avaiya <hav.hmrtech@gmail.com>
2025-06-30 18:11:42 +0530

    set profile picture & logout feature

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const DATABASE_URL = process.env.DATABASE_URL;
 app.use(cors());
 
 
-//app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", express.json(), authRoutes);
 app.use("/api/posts", express.json(), postRoutes);
